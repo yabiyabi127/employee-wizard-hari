e2a5fb8355eea9c78dcd4a781e12266a7c106c47
hari <yabiyabi127@gmail.com>
2026-01-17 08:13:51 +0700

    chore: initialize React project with Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    globals: true,
+    testTimeout: 15000
+  },
+});
